5e8d1a5d7e3a9c19b41ae1ac7088fdf933c4db3a
Viktar Krauchyk <5809909@tut.by>
2017-12-27 16:26:27 +0300

    1st

---
+++ app/src/main/java/com/github/a5809909/gps_finder/MainActivity.java
@@ -0,0 +1,256 @@
+package com.github.a5809909.gps_finder;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.CellLocation;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private static final int LOCATION_PERMISSION_CODE = 855;
+    private static final String API_KEY = "AIzaSyDNsRNkiJddjICdCY9fiFw3U6_nziORLC4";
+
+    private MainActivity instance;
+    private static final String TAG = "Main";
+    //    private DatabaseHelper databaseHelper;
+    private TextView lbsLatitude, lbsLongtitude, lbsAltitude, lbsPrecision, lbsType;
+
+    public static void requestStoragePermissions(Activity activity, int PERMISSION_REQUEST_CODE) {
+        java.lang.String[] perms = {"android.permission.ACCESS_FINE_LOCATION", "android.permission.ACCESS_COARSE_LOCATION", "android.permission.READ_PHONE_STATE"};
+        ActivityCompat.requestPermissions(activity, perms, PERMISSION_REQUEST_CODE);
+    }
+
+    public static boolean isLocationPermissionsAllowed(Activity activity) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            return activity.checkSelfPermission("android.permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_GRANTED
+                    &&
+                    activity.checkSelfPermission("android.permission.ACCESS_COARSE_LOCATION") == PackageManager.PERMISSION_GRANTED
+                    &&
+                    activity.checkSelfPermission("android.permission.READ_PHONE_STATE") == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instance = this;
+        initViews();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_do_lbs:
+                Toast.makeText(this, "sdfsdfsd", Toast.LENGTH_LONG).show();
+                getLocationClicked(v);
+
+                break;
+            case R.id.btn_show_database:
+                Intent intentDataBase = new Intent(getApplicationContext(), DataActivity.class);
+                startActivity(intentDataBase);
+
+                break;
+            case R.id.btn_maps:
+                Intent intentMap = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intentMap);
+
+                break;
+
+        }
+    }
+
+    public void startService() {
+        Log.d(TAG, "startService() called");
+        Calendar cal = Calendar.getInstance();
+//        Intent intent = new Intent(this, LoggerService.class);
+//        PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
+//        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),60 * 1000, pintent);
+    }
+
+    public void getLocationClicked(View v) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (isLocationPermissionsAllowed(this)) {
+
+            } else {
+                requestStoragePermissions(this, LOCATION_PERMISSION_CODE);
+
+            }
+
+            TelephonyManager tel = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+
+            if (tel != null) {
+                CellLocation loc = tel.getCellLocation();
+
+                if ((loc != null) && (loc instanceof GsmCellLocation)) {
+                    GsmCellLocation gsmLoc = (GsmCellLocation) loc;
+                    String op = tel.getNetworkOperator();
+
+                    String cid = "" + gsmLoc.getCid();
+                    String lac = "" + gsmLoc.getLac();
+                    String mcc = op.substring(0, 3);
+                    String mnc = op.substring(3);
+                    new HttpPostTask().execute(cid, lac, mcc, mnc);
+                } else {
+                    Toast.makeText(instance, "No valid GSM network found",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private class HttpPostTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog pd;
+
+        @Override
+        protected void onPreExecute() {
+            pd = new ProgressDialog(instance);
+            pd.setTitle("Getting Location");
+            pd.setMessage("Please Wait...");
+            pd.setCancelable(false);
+            pd.setIndeterminate(true);
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String result = null;
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpost = new HttpPost("https://www.googleapis.com/geolocation/v1/geolocate?key=" + API_KEY);
+
+            StringEntity se;
+
+            try {
+                JSONObject cellTower = new JSONObject();
+                cellTower.put("cellId", params[0]);
+                cellTower.put("locationAreaCode", params[1]);
+                cellTower.put("mobileCountryCode", params[2]);
+                cellTower.put("mobileNetworkCode", params[3]);
+                Log.i(TAG, "cellId: " + params[0] +
+                        ", locationAreaCode: " + params[1] +
+                        ", mobileCountryCode: " + params[2] +
+                        ", mobileNetworkCode: " + params[3]);
+                JSONArray cellTowers = new JSONArray();
+                cellTowers.put(cellTower);
+
+                JSONObject rootObject = new JSONObject();
+                rootObject.put("cellTowers", cellTowers);
+
+                se = new StringEntity(rootObject.toString());
+                se.setContentType("application/json");
+
+                httpost.setEntity(se);
+                httpost.setHeader("Accept", "application/json");
+                httpost.setHeader("Content-type", "application/json");
+                Log.i(TAG, "se: " + se);
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                String response = httpclient.execute(httpost, responseHandler);
+
+                result = response;
+                Log.i(TAG, "ssseee: " + se);
+            } catch (Exception e) {
+                final String err = e.getMessage();
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(instance, "Exception requesting location: " + err, Toast.LENGTH_LONG).show();
+                    }
+
+                });
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (pd != null) {
+                try {
+                    pd.dismiss();
+                } catch (Exception e) {
+                }
+            }
+
+            if (result != null) {
+                try {
+                    JSONObject jsonResult = new JSONObject(result);
+                    JSONObject location = jsonResult.getJSONObject("location");
+                    String lat, lng;
+
+                    lat = location.getString("lat");
+                    lng = location.getString("lng");
+                    lbsAltitude.setText(lat);
+                    lbsLongtitude.setText(lng);
+
+                } catch (Exception e) {
+                    Toast.makeText(instance, "Exception parsing response: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private void initViews() {
+        lbsLatitude = findViewById(R.id.lbs_latitude);
+        lbsLongtitude = findViewById(R.id.lbs_longtitude);
+        lbsAltitude = findViewById(R.id.lbs_altitude);
+        lbsPrecision = findViewById(R.id.lbs_precision);
+        lbsType = findViewById(R.id.lbs_type);
+
+    }
+
+//    private void saveInSql(PhoneState result) {
+//        databaseHelper = new DatabaseHelper(this);
+//        databaseHelper.addUser(result);
+//
+//    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
