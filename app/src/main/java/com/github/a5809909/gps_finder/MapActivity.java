b24e881e5db375be9856876fffc2c4f38774e727
bektop <5809909@mail.ru>
2017-12-27 20:31:08 +0300

    map shows india

--- app/src/main/java/com/github/a5809909/gps_finder/MapActivity.java
+++ app/src/main/java/com/github/a5809909/gps_finder/MapActivity.java
@@ -1,118 +1,38 @@
 package com.github.a5809909.gps_finder;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import android.support.design.widget.FloatingActionButton;
-
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
-    public void onMapReady(GoogleMap pGoogleMap) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
 
+        initMap();
     }
 
-    public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private void initMap() {
 
-        SupportMapFragment mapFragment;
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    @Override
+    public void onMapReady(GoogleMap map) {
 
-
-            mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
-                            googleMap.addMarker(new MarkerOptions()
-                                    .position(new LatLng(37.4233438, -122.0728817))
-                                    .title("LinkedIn")
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-
-                            googleMap.addMarker(new MarkerOptions()
-                                    .position(new LatLng(37.4629101,-122.2449094))
-                                    .title("Facebook")
-                                    .snippet("Facebook HQ: Menlo Park"));
-
-                            googleMap.addMarker(new MarkerOptions()
-                                    .position(new LatLng(37.3092293, -122.1136845))
-                                    .title("Apple"));
-
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.4233438, -122.0728817), 10));
-                        }
-                    });
-                }
-            });
-
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(37.4233438, -122.0728817))
-                    .title("LinkedIn")
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(37.4629101,-122.2449094))
-                    .title("Facebook")
-                    .snippet("Facebook HQ: Menlo Park"));
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(37.3092293, -122.1136845))
-                    .title("Apple"));
-
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.4233438, -122.0728817), 10));
-        }
+        LatLng latLng = new LatLng(13.05241, 80.25082);
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.addMarker(new MarkerOptions().position(latLng).title("Raj Amal"));
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
     }
 }
