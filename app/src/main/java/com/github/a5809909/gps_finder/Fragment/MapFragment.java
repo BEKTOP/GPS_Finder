4ee36aa7407cbbed569a165018cf28cd6fd6d3c0
bektop <5809909@mail.ru>
2018-01-06 02:18:06 +0300

    map shows lat lng

--- app/src/main/java/com/github/a5809909/gps_finder/Fragment/MapFragment.java
+++ app/src/main/java/com/github/a5809909/gps_finder/Fragment/MapFragment.java
@@ -1,6 +1,9 @@
 package com.github.a5809909.gps_finder.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.InflateException;
 import android.view.LayoutInflater;
@@ -8,9 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.a5809909.gps_finder.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private SharedPreferences sPref;
+    private MapView mapView;
 
     public MapFragment() {
         // Required empty public constructor
@@ -34,7 +47,39 @@ public class MapFragment extends Fragment{
             view = inflater.inflate(R.layout.fragment_map, container, false);
         } catch (InflateException e) {
         }
+
+
         return view;
     }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView =  view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        SharedPreferences sPref = this.getActivity().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        String savedLat = sPref.getString("lat", "");
+        String savedLng = sPref.getString("lng", "");
+        double lat = Double.parseDouble(savedLat);
+        double lng = Double.parseDouble(savedLng);
+        LatLng latLng = new LatLng(lat, lng);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)
+                .zoom(16)
+                .build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        map.animateCamera(cameraUpdate);
+        map.addMarker(new MarkerOptions().position(latLng).title("lat:" + savedLat + ", lng:" + savedLng));
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
 
 }
